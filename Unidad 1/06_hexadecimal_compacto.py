2d07dca60ee4fdfaadd849575a99750835da3f18
Alejandra Sierra Robles <sierraalejandra323@gmail.com>
2026-09-01 11:52:49 -0600

    unidad 2

--- Unidad 1/06_hexadecimal_compacto.py
+++ Unidad 1/06_hexadecimal_compacto.py
@@ -1,7 +1,7 @@
-numero = 10; hexadecimal = "0" if numero == 0 else ""; 
-while numero > 0: 
-    residuo = numero % 16; 
-    hexadecimal = ("ABCDEF"[residuo - 10] if residuo >= 10 else str(residuo)) + hexadecimal; 
+numero = 10
+hexadecimal = "0" if numero == 0 else ""
+while numero > 0:
+    residuo = numero % 16
+    hexadecimal = ("ABCDEF"[residuo - 10] if residuo >= 10 else str(residuo)) + hexadecimal
     numero //= 16
 print(hexadecimal)
-
--- Unidad 2/02_operadores.py
+++ Unidad 2/02_operadores.py
@@ -1,6 +1,6 @@
-operador=input
-
-O1=input int
-O2=input int
-
-resultado=O1+operador
+operador = input("inserta un operador (+,-,*,/): ")
+if operador == "+":
+    O1 = input("ingresa un numero: ")
+    O2 = input("ingresa otro numero: ")
+    resultado =(O1) + operador + (O2)
+    print(eval(resultado))
