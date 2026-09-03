126e45d1acd87737cafa4e629e2371b376c0baa4
Alejandra Sierra Robles <sierraalejandra323@gmail.com>
2026-09-03 11:54:20 -0600

    Create

---
+++ Unidad 2/03_for.py
@@ -0,0 +1,15 @@
+#trabajando cocon rangos de for
+for numero in range (1,6,2):
+    cuadrado = numero ** 2
+    print(numero, cuadrado)
+
+#trabajando con listas de for
+materias= ["Python", "Lynux", "interfaces"]
+for materia in materias:
+    print(materia)
+cadena="0123456789ABCDEF"
+for letre in cadena:
+    print(letre)
+
+for i in range (len(cadena)):
+    print((cadena[i]))
